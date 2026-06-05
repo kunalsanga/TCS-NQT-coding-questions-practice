fd6d456e3ec918273124ce69e2bbea4804bec688
kunalsanga <kunalsanga197@gmail.com>
2026-06-05 17:34:25 +0530

    add

---
+++ campus MONK/6_common_prefix.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<string> strs = {"flower","flow","flight"};
+    string prefix = strs[0];
+    for(int i = 1; i<strs.size();i++){
+        while(strs[i].find(prefix)!=0){
+            prefix.pop_back();
+
+            if(prefix.empty()){
+                cout<<"";
+                return 0;
+            }
+        }
+    }
+    cout<<prefix;
+    return 0;
+}
