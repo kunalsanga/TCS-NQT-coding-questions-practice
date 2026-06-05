33e01b5c2a4e4beb7dbc0ad1c326493a751ad806
kunalsanga <kunalsanga197@gmail.com>
2026-06-05 13:17:28 +0530

    add

---
+++ campus MONK/1_hash_star.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+
+    int stars=0;
+    int hashes=0;
+
+    for(char ch: s){
+        if(ch =='*'){
+            stars++;
+        }else if(ch =='#'){
+            hashes++;
+        }
+    }
+    cout<<stars-hashes;
+    return 0;
+}
