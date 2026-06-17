78c71649ccf08d95b15cac2dd9b19d33a1ad8e50
kunalsanga <kunalsanga197@gmail.com>
2026-06-17 21:12:07 +0530

    add

---
+++ campus MONK/TCS Actual Coding question/2_sum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==target){
+                cout<<i<<" "<<j;
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
